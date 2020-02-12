f4989876ce410903f03c1149c2051188afdc4c55
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-12 00:00:19 -0500

    Fetch users from the database

--- web_app/app.py
+++ web_app/app.py
@@ -36,12 +36,27 @@ def about():
 @app.route("/users")
 @app.route("/users.json")
 def users():
-    users = [
-        {"id":1, "name": "First User"},
-        {"id":2, "name": "Second User"},
-        {"id":3, "name": "Third User"},
-    ]
-    return jsonify(users)
+    #users = [
+    #    {"id":1, "name": "First User"},
+    #    {"id":2, "name": "Second User"},
+    #    {"id":3, "name": "Third User"},
+    #]
+    #return jsonify(users)
+
+    users = User.query.all() # returns a list of <class 'alchemy.User'>
+    print(type(users))
+    print(type(users[0]))
+    #print(len(users))
+
+    users_response = []
+    for u in users:
+        user_dict = u.__dict__
+        del user_dict["_sa_instance_state"]
+        users_response.append(user_dict)
+
+    return jsonify(users_response)
+
+
 
 @app.route("/users/create", methods=["POST"])
 def create_user():
