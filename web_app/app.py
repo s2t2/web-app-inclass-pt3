3b4fd515ef008b2de0c9fb2b16d3c4719ab20ae5
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-13 20:07:48 -0500

    App factory

--- web_app/app.py
+++ web_app/app.py
@@ -3,92 +3,102 @@ from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config["CUSTOM_VAR"] = 5 # just an example of app config :-D
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_200.db"
+def create_app():
+    #load_dotenv()
+    #app_env = os.environ.get("FLASK_ENV", "development") # set to "production" in the production environment
+    #secret_key = os.environ.get("SECRET_KEY", "my super secret") # overwrite this in the production environment
+    #testing = False # True if app_env == "test" else False
+    #app = Flask(__name__, instance_relative_config=True)
+    #app.config.from_mapping(ENV=app_env, SECRET_KEY=secret_key, TESTING=testing)
+    #app.register_blueprint(home_routes)
+    #app.register_blueprint(product_routes)
 
-db = SQLAlchemy(app)
+    app = Flask(__name__)
+    app.config["CUSTOM_VAR"] = 5 # just an example of app config :-D
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_200.db"
 
-migrate = Migrate(app, db)
+    db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+    migrate = Migrate(app, db)
 
-class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(128))
 
-#
-# ROUTING
-#
+    class Tweet(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        status = db.Column(db.String)
+        user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-@app.route("/")
-def index():
-    #return "Hello World!"
-    return render_template("homepage.html")
+    #
+    # ROUTING
+    #
 
-@app.route("/about")
-def about():
-    return "About Me"
+    @app.route("/")
+    def index():
+        #return "Hello World!"
+        return render_template("homepage.html")
 
-@app.route("/users")
-@app.route("/users.json")
-def users():
-    #users = [
-    #    {"id":1, "name": "First User"},
-    #    {"id":2, "name": "Second User"},
-    #    {"id":3, "name": "Third User"},
-    #]
-    #return jsonify(users)
+    @app.route("/about")
+    def about():
+        return "About Me"
 
-    users = User.query.all() # returns a list of <class 'alchemy.User'>
-    print(type(users))
-    print(type(users[0]))
-    #print(len(users))
+    @app.route("/users")
+    @app.route("/users.json")
+    def users():
+        #users = [
+        #    {"id":1, "name": "First User"},
+        #    {"id":2, "name": "Second User"},
+        #    {"id":3, "name": "Third User"},
+        #]
+        #return jsonify(users)
 
-    users_response = []
-    for u in users:
-        user_dict = u.__dict__
-        del user_dict["_sa_instance_state"]
-        users_response.append(user_dict)
+        users = User.query.all() # returns a list of <class 'alchemy.User'>
+        print(type(users))
+        print(type(users[0]))
+        #print(len(users))
 
-    return jsonify(users_response)
+        users_response = []
+        for u in users:
+            user_dict = u.__dict__
+            del user_dict["_sa_instance_state"]
+            users_response.append(user_dict)
 
-
-
-@app.route("/users/create", methods=["POST"])
-def create_user():
-    print("CREATING A NEW USER...")
-    print("FORM DATA:", dict(request.form))
-    # todo: create a new user
-    #return jsonify({"message": "CREATED OK (TODO)"})
-    if "name" in request.form:
-        name = request.form["name"]
-        print(name)
-        db.session.add(User(name=name))
-        db.session.commit()
-        return jsonify({"message": "CREATED OK", "name": name})
-    else:
-        return jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"})
+        return jsonify(users_response)
 
 
 
+    @app.route("/users/create", methods=["POST"])
+    def create_user():
+        print("CREATING A NEW USER...")
+        print("FORM DATA:", dict(request.form))
+        # todo: create a new user
+        #return jsonify({"message": "CREATED OK (TODO)"})
+        if "name" in request.form:
+            name = request.form["name"]
+            print(name)
+            db.session.add(User(name=name))
+            db.session.commit()
+            return jsonify({"message": "CREATED OK", "name": name})
+        else:
+            return jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"})
 
-# GET /hello
-# GET /hello?name=Polly
-@app.route("/hello")
-def hello(name=None):
-    print("VISITING THE HELLO PAGE")
-    print("REQUEST PARAMS:", dict(request.args))
+    # GET /hello
+    # GET /hello?name=Polly
+    @app.route("/hello")
+    def hello(name=None):
+        print("VISITING THE HELLO PAGE")
+        print("REQUEST PARAMS:", dict(request.args))
 
-    if "name" in request.args:
-        name = request.args["name"]
-        message = f"Hello, {name}"
-    else:
-        message = "Hello World"
+        if "name" in request.args:
+            name = request.args["name"]
+            message = f"Hello, {name}"
+        else:
+            message = "Hello World"
 
-    #return message
-    return render_template("hello.html", message=message)
+        #return message
+        return render_template("hello.html", message=message)
+
+
+    return app
