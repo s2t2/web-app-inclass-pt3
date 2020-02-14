b04b3cb9efb78e1fd0e928b15b42b08755fbebf3
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-13 21:03:14 -0500

    Routes blueprint

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,12 @@
 
 import os
 from dotenv import load_dotenv
-from flask import Flask, jsonify, request, render_template
+from flask import Flask #, jsonify, request, render_template
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
 
 from web_app.models import db, User, Tweet, migrate
+from web_app.routes import routes
 
 DATABASE_URL = os.getenv("DATABASE_URL", default="OOPS")
 
@@ -32,72 +33,6 @@ def create_app():
     #with app.app_context():
     #    db.create_all()
 
-    #
-    # ROUTING
-    #
-
-    @app.route("/")
-    def index():
-        #return "Hello World!"
-        return render_template("homepage.html")
-
-    @app.route("/about")
-    def about():
-        return "About Me"
-
-    @app.route("/users")
-    @app.route("/users.json")
-    def get_users():
-        #users = [
-        #    {"id":1, "name": "First User"},
-        #    {"id":2, "name": "Second User"},
-        #    {"id":3, "name": "Third User"},
-        #]
-        #return jsonify(users)
-
-        users = User.query.all() # returns a list of <class 'alchemy.User'>
-        print(type(users))
-        #print(type(users[0]))
-        #print(len(users))
-
-        users_response = []
-        for u in users:
-            user_dict = u.__dict__
-            del user_dict["_sa_instance_state"]
-            users_response.append(user_dict)
-
-        return jsonify(users_response)
-
-    @app.route("/users/create", methods=["POST"])
-    def create_user():
-        print("CREATING A NEW USER...")
-        print("FORM DATA:", dict(request.form))
-        # todo: create a new user
-        #return jsonify({"message": "CREATED OK (TODO)"})
-        if "name" in request.form:
-            name = request.form["name"]
-            print(name)
-            db.session.add(User(name=name))
-            db.session.commit()
-            return jsonify({"message": "CREATED OK", "name": name})
-        else:
-            return jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"})
-
-    # GET /hello
-    # GET /hello?name=Polly
-    @app.route("/hello")
-    def hello(name=None):
-        print("VISITING THE HELLO PAGE")
-        print("REQUEST PARAMS:", dict(request.args))
-
-        if "name" in request.args:
-            name = request.args["name"]
-            message = f"Hello, {name}"
-        else:
-            message = "Hello World"
-
-        #return message
-        return render_template("hello.html", message=message)
-
+    app.register_blueprint(routes)
 
     return app
---
+++ web_app/routes.py
@@ -0,0 +1,74 @@
+
+
+from flask import Blueprint, jsonify, request, render_template, current_app
+
+from web_app.models import User, Tweet, db
+
+routes = Blueprint("routes", __name__)
+
+#
+# ROUTING
+#
+
+@routes.route("/")
+def index():
+    #return "Hello World!"
+    return render_template("homepage.html")
+
+@routes.route("/about")
+def about():
+    return "About Me"
+
+@routes.route("/users")
+@routes.route("/users.json")
+def get_users():
+    #users = [
+    #    {"id":1, "name": "First User"},
+    #    {"id":2, "name": "Second User"},
+    #    {"id":3, "name": "Third User"},
+    #]
+    #return jsonify(users)
+
+    users = User.query.all() # returns a list of <class 'alchemy.User'>
+    print(type(users))
+    #print(type(users[0]))
+    #print(len(users))
+
+    users_response = []
+    for u in users:
+        user_dict = u.__dict__
+        del user_dict["_sa_instance_state"]
+        users_response.append(user_dict)
+
+    return jsonify(users_response)
+
+@routes.route("/users/create", methods=["POST"])
+def create_user():
+    print("CREATING A NEW USER...")
+    print("FORM DATA:", dict(request.form))
+    # todo: create a new user
+    #return jsonify({"message": "CREATED OK (TODO)"})
+    if "name" in request.form:
+        name = request.form["name"]
+        print(name)
+        db.session.add(User(name=name))
+        db.session.commit()
+        return jsonify({"message": "CREATED OK", "name": name})
+    else:
+        return jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"})
+
+# GET /hello
+# GET /hello?name=Polly
+@routes.route("/hello")
+def hello(name=None):
+    print("VISITING THE HELLO PAGE")
+    print("REQUEST PARAMS:", dict(request.args))
+
+    if "name" in request.args:
+        name = request.args["name"]
+        message = f"Hello, {name}"
+    else:
+        message = "Hello World"
+
+    #return message
+    return render_template("hello.html", message=message)
