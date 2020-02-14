f005eb83ac0331888b81c21a2f969f119edb5fea
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-13 23:08:49 -0500

    Get elon full tweets

--- web_app/twitter_service.py
+++ web_app/twitter_service.py
@@ -18,10 +18,21 @@ auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 
 client = tweepy.API(auth)
 print(type(client))
-print(dir(client))
-print("----------")
+#print(dir(client))
 
-public_tweets = client.home_timeline()
+#print("----------")
+#public_tweets = client.home_timeline()
+#for tweet in public_tweets:
+#    print(type(tweet), tweet.text)
 
-for tweet in public_tweets:
-    print(type(tweet), tweet.text)
+
+#print("----------------")
+#elon_tweets = client.user_timeline("elonmusk")
+#for tweet in elon_tweets:
+#    print(type(tweet), tweet.text)
+
+
+print("----------------")
+elon_tweets = client.user_timeline("elonmusk", tweet_mode="extended")
+for tweet in elon_tweets:
+    print(type(tweet), tweet.full_text)
